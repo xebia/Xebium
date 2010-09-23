3703c45cc9c99519c8a32d1aee00f83d257b03f5
amolenaar <amolenaar@34dc7d9b-b005-0410-8eda-a134d535b8ea>
2010-09-23 10:13:56 +0000

    More logging from fixture
    git-svn-id: https://svn.xebia.com/repos/projects/xke/pitches/xebium/trunk@11437 34dc7d9b-b005-0410-8eda-a134d535b8ea

--- src/main/java/com/xebia/incubator/xebium/SeleniumScriptFixture.java
+++ src/main/java/com/xebia/incubator/xebium/SeleniumScriptFixture.java
@@ -34,22 +34,28 @@ public class SeleniumScriptFixture {
 
 		String result = null;
 		try {
+			LOG.info("Server started, launching test suite");
 			HTMLLauncher launcher = new HTMLLauncher(remoteControl);
 			//try {
-				result = launcher.runHTMLSuite(browser, browserURL, suiteFile, outputFile, timeoutInSeconds, multiWindow);
+			result = launcher.runHTMLSuite(browser, browserURL, suiteFile, outputFile, timeoutInSeconds, multiWindow);
 			//} catch (SeleniumCommandTimedOutException e) {
 			//	LOG.error("HTML suite failed with timeout", e);
 			//}
+			LOG.info("Finished execution of test suite, result = " + result);
+		} catch (Exception e) {
+			LOG.error("Failed to run test suite", e);
+			throw e;
 		} finally {
 			// TODO: do something with result
 			remoteControl.stop();
 		}
 		
+		LOG.debug("End of RunScript");
 		return result;
 	}
 
 	private File asFile(final String scriptName) {
-		String fileName = scriptName.replaceAll("http:", "FitNesseRoot");
+		String fileName = scriptName.replaceAll("http:/", "FitNesseRoot");
 		
 		return new File(fileName);
 	}
