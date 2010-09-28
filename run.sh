5c4071bbf593f4b2be7c67fa4fc3af46faba04fb
amolenaar <amolenaar@34dc7d9b-b005-0410-8eda-a134d535b8ea>
2010-09-28 19:21:41 +0000

    simplified run.sh
    git-svn-id: https://svn.xebia.com/repos/projects/xke/pitches/xebium/trunk@11548 34dc7d9b-b005-0410-8eda-a134d535b8ea

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # Make sure we have our jars close at hand.
-mvn dependency:copy-dependencies
+#mvn dependency:copy-dependencies
 
 # Standard options:
 # -o -e 0
 
 
-java -Xmx256m -DmavenRepo="${M2_REPO}" -cp ${M2_REPO}/org/fitnesse/fitnesse/20100303/fitnesse-20100303.jar fitnesseMain.FitNesseMain -o -e 0 $1 $2 $3 $4 $5
+#java -Xmx256m -DmavenRepo="${M2_REPO}" -cp ${M2_REPO}/org/fitnesse/fitnesse/20100303/fitnesse-20100303.jar fitnesseMain.FitNesseMain -o -e 0 $1 $2 $3 $4 $5
+mvn -Pfitnesse test
